06179ec160c9752ad966d38d0405a6d8c2f15a1c
Singarapuprasannakumar <prasannakumar.singarapu@sasi.ac.in>
2026-07-01 09:55:14 +0530

    chore: accept both MONGO_URI and MONGODB_URI

--- src/shared/services/db-connection.ts
+++ src/shared/services/db-connection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/community-hero";
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI || "mongodb://localhost:27017/community-hero";
 
 interface MongooseCache {
   conn: typeof mongoose | null;
